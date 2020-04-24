49c2bba937f96eadf450aacb47737186821f0dac
McClain-Thiel <mcclain.thiel@gmail.com>
2020-04-24 14:19:54 -0700

    i think the wrapper generator is a good path

--- Data.py
+++ Data.py
@@ -11,7 +11,7 @@ class DataLoader(object):
 
         Possible ideas:
             1.) wrapper generator that takes only a few images and fills the rest of
-            the batch with perturbed images. Might slow down data movment by a lot. 
+            the batch with perturbed images. Might slow down data movment by a lot.
 
 
     Dataloader class with attributes:
@@ -19,6 +19,8 @@ class DataLoader(object):
             val_gen: same as above, but with validation data
             val_keys: pd.Dataframe that has the classes of the validation set.
                      I honestly don't know the format of this DF
+            adv_gen: generator that spits out a collection of perterbed images
+                    in the same form as the normal generator
 
     Initialize with:
         Dataloader(train_file_path, val_file_path, batch_size, input_shape)
@@ -39,3 +41,20 @@ class DataLoader(object):
                                                   shuffle=True,
                                                   target_size=input_shape)
         self.val_keys = pd.read_csv(val_file_path + '/' + keys_file, sep='	', header=None)
+
+
+    def perterb_img(self, img):
+        #https://github.com/tensorflow/models/blob/master/research/adversarial_logit_pairing/adversarial_attack.py
+        raise NotImplementedError
+
+    def build_perberbed_batch(self, original_batch):
+        raise NotImplementedError
+
+    def adv_gen(self):
+        """
+        this is the actual generator
+        :return: tuple
+            0: image data in shape (batch_size, (input_shape), color_channel)
+            1: one hot encoded label
+        """
+        raise NotImplementedError
